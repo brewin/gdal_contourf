944c35643792f51d716833f0aab7ba20523f648c
brewin <brewin@gmail.com>
2019-10-16 12:49:35 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
     `maven-publish`
     kotlin("jvm") version "1.3.50"
     id("com.github.johnrengelman.shadow") version "5.1.0"
-    id("com.github.ben-manes.versions") version "0.22.0"
+    id("com.github.ben-manes.versions") version "0.27.0"
 }
 
 group = "com.github.brewin"
-version = "1.1"
+version = "1.2"
 
 repositories {
     jcenter()
