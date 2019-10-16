2bbb10179c7e2ec0911cbe698a40657291f01cc6
brewin <brewin@gmail.com>
2019-10-16 12:47:27 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     `java-library`
+    `maven-publish`
     kotlin("jvm") version "1.3.50"
     id("com.github.johnrengelman.shadow") version "5.1.0"
     id("com.github.ben-manes.versions") version "0.22.0"
