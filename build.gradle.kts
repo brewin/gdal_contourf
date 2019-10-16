7782aecdf5cd2f95a2bb3964d369cde7ffcddec7
Braden Ewing <brewin@gmail.com>
2019-10-16 13:21:21 -0500

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     `java-library`
-    `maven-publish`
+    maven
     kotlin("jvm") version "1.3.50"
     id("com.github.johnrengelman.shadow") version "5.1.0"
     id("com.github.ben-manes.versions") version "0.27.0"
