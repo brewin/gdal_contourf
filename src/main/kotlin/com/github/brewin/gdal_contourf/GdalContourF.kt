9225feae21a340f54f1ec3f7a919c8d0ed32a7ec
Braden Ewing <brewin@gmail.com>
2020-05-20 18:49:57 -0500

    Add missing dash

--- src/main/kotlin/com/github/brewin/gdal_contourf/GdalContourF.kt
+++ src/main/kotlin/com/github/brewin/gdal_contourf/GdalContourF.kt
@@ -199,7 +199,7 @@ object GdalContourF {
 
     private fun mapArgs(args: Array<String>): Map<String, List<String>> =
         args.fold(Pair(emptyMap<String, List<String>>(), "")) { (map, lastKey), elem ->
-            if (elem.startsWith("-")) Pair(map + (elem to emptyList()), elem)
+            if (elem.startsWith("--")) Pair(map + (elem to emptyList()), elem)
             else Pair(map + (lastKey to map.getOrDefault(lastKey, emptyList()) + elem), lastKey)
         }.first
 
